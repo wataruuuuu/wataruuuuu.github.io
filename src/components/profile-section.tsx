436ac93001d1ec99c563f670108357af9766feb1
wataru <ikeda.wataru.q5@dc.tohoku.ac.jp>
2025-03-09 20:42:07 +0900

    image to square

--- src/components/profile-section.tsx
+++ src/components/profile-section.tsx
@@ -22,7 +22,7 @@ export function ProfileSection({ aboutMe }: ProfileSectionProps) {
     <div className="md:sticky top-12 flex flex-row-reverse md:flex-col gap-4 md:space-y-8">
       {aboutMe.imageUrl && (
         <div className="w-1/3 md:w-full flex-shrink-0">
-          <div className="relative max-h-[45vh] md:w-[65%] aspect-[3/4]">
+          <div className="relative max-h-[45vh] md:w-[65%] aspect-square">
             <Image
               src={aboutMe.imageUrl}
               alt={aboutMe.name}
