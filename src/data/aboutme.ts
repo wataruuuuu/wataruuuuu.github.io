65df770e4e099486fcd5ccb9c397064b60686c76
wataru <ikeda.wataru.q5@dc.tohoku.ac.jp>
2025-03-09 20:15:44 +0900

    Updated personal information

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,19 +18,19 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Wataru Ikeda / 池田 航",
+  title: "4th Year Undergraduate Student",
+  institution: "Tohoku University",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+  "I'm a 4th year undergraduate student at <a href='https://www.tohoku.ac.jp/en/'>Tohoku University</a>. My research interests include [your research interests/field].",
+  email: "ikeda.wataru.q5@dc.tohoku.ac.jp",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
+    "https://gyazo.com/8a639c037eea594c60eb2f24efc2086c",
+  //googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
+  githubUsername: "wataruuuuu",
+  //linkedinUsername: "janesmith",
+  twitterUsername: "jkliplop",
   blogUrl: "https://",
   cvUrl: "https://",
   institutionUrl: "https://www.stanford.edu",
