4cae70b9a01454d9b13edd8e5442f0bafd22f3c6
wataru <ikeda.wataru.q5@dc.tohoku.ac.jp>
2025-12-02 11:21:27 -0800

    update news

--- src/data/news.ts
+++ src/data/news.ts
@@ -6,11 +6,22 @@ export interface News {
 }
 
 export const newsData: News[] = [
-  // If you don't want to show news, just make the array empty.
-  /*{
-    date: "March 2024",
-    title: "Paper accepted at ICML 2024",
-    description: "Our work on causal discovery in time series data has been accepted at ICML 2024.",
-    link: "https://icml.cc/",
-  }*/
+  {
+    date: "Nov 26, 2025",
+    title: "Prize Winner of NeurIPS 2025 Workshop",
+    description: "Our team has been selected as a prize winner in the MMU-RAG NeurIPS 2025 Competition.",
+    link: "https://neurips.cc/virtual/2025/loc/san-diego/competition/127730",
+  },
+  {
+    date: "Jul 7, 2025",
+    title: "Paper accepted at COLM 2025",
+    description: "Our paper, \"Layerwise Importance Analysis of Feed-Forward Networks in Transformer-based Language Models,\" has been accepted at COLM 2025.",
+    link: "https://colm.org/",
+  },
+  {
+    date: "Mar 13, 2025",
+    title: "",
+    description: "言語処理学会第31回年次大会 (ANLP 2025) にて若手奨励賞を受賞しました。",
+    link: "https://anlp.jp/nlp2025/award.html",
+  }
 ];
